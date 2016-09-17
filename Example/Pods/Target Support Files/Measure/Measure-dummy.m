435abe31ba67220c8d76ca91f884e902d7e62e61
muukii <m@muukii.me>
2016-09-18 01:23:58 +0900

    Add Pods

---
+++ Example/Pods/Target Support Files/Measure/Measure-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Measure : NSObject
+@end
+@implementation PodsDummy_Measure
+@end
---
+++ Example/Pods/Target Support Files/Pods-Measure_Tests/Pods-Measure_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Measure_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Measure_TestsVersionString[];
+
